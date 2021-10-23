d89f5df1f273e8eecfc3935ded0ce37ae60ffaf2
LayMui <mui_ume@hotmail.com>
2021-10-23 17:42:11 +0800

    Add data-testid for alert message

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6 offset-sm-3">
-                    <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
+                    <div data-testid="alert" v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
                     <router-view></router-view>
                 </div>
             </div>
